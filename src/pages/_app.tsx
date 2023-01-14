712ee8348a67827d14c81ed0e85bc794724a1d7e
mac <mharrvic@gmail.com>
2023-01-14 18:05:27 +0800

    Add precedent packages, styles, and components by steven tey

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,24 @@
-import { type AppType } from "next/app";
+import { Inter } from "@next/font/google";
+import localFont from "@next/font/local";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
+import { Provider as RWBProvider } from "react-wrap-balancer";
 
 import { api } from "../utils/api";
 
+import clsx from "clsx";
 import "../styles/globals.css";
+
+const sfPro = localFont({
+  src: "../styles/SF-Pro-Display-Medium.otf",
+  variable: "--font-sf",
+});
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +26,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <RWBProvider>
+        <main className={clsx(sfPro.variable, inter.variable)}>
+          <Component {...pageProps} />
+        </main>
+      </RWBProvider>
     </SessionProvider>
   );
 };
