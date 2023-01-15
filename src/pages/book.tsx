f0989d4c997c60cf7ab2a74be3879f449e5e5b20
mac <mharrvic@gmail.com>
2023-01-15 14:07:02 +0800

    Add book page

---
+++ src/pages/book.tsx
@@ -0,0 +1,42 @@
+import Search from "@/components/home/search";
+import Layout from "@/components/layout/layout";
+import { FADE_DOWN_ANIMATION_VARIANTS } from "@/lib/constants";
+import { motion } from "framer-motion";
+import Balancer from "react-wrap-balancer";
+
+export default function Home() {
+  return (
+    <Layout>
+      <motion.div
+        className="max-w-xl px-5 xl:px-0"
+        initial="hidden"
+        whileInView="show"
+        animate="show"
+        viewport={{ once: true }}
+        variants={{
+          hidden: {},
+          show: {
+            transition: {
+              staggerChildren: 0.15,
+            },
+          },
+        }}
+      >
+        <motion.h1
+          className="bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent drop-shadow-sm md:text-7xl md:leading-[5rem]"
+          variants={FADE_DOWN_ANIMATION_VARIANTS}
+        >
+          <Balancer>Turn Books into Playlist</Balancer>
+        </motion.h1>
+        <motion.p
+          className="mt-6 text-center text-gray-500 md:text-xl"
+          variants={FADE_DOWN_ANIMATION_VARIANTS}
+        >
+          <Balancer>Search any book title to get a playlist of songs</Balancer>
+        </motion.p>
+      </motion.div>
+
+      <Search />
+    </Layout>
+  );
+}
