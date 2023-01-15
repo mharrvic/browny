88e53d715898207fa94289af39e9d81f22520503
mac <mharrvic@gmail.com>
2023-01-15 14:05:30 +0800

    Add books google images

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const config = {
     defaultLocale: "en",
   },
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    domains: ["lh3.googleusercontent.com", "books.google.com"],
   },
 };
 export default config;
