83211bf94014441cb2b670c09ddd4ed620646572
mac <mharrvic@gmail.com>
2023-01-17 00:59:04 +0800

    Add openai util

---
+++ src/utils/openai.ts
@@ -0,0 +1,27 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const MODEL = "text-davinci-003";
+
+export async function generateSongList(title: string, description: string) {
+  const songs = 10;
+  const response = await openai.createCompletion({
+    model: MODEL,
+    prompt: `Instructions:
+      \n- Generate a top ${songs} list of songs playlist that is related to the "Book Title: " and "Description: "
+      \n\nBook Title: ${title}  
+      \nDescription: ${description}
+      \n\n\nPlaylist: `,
+    temperature: 0.7,
+    max_tokens: 512,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  return response.data;
+}
