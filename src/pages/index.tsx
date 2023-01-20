58feaa0b10939b7fefc24b4da469be70efb4e8ca
mac <mharrvic@gmail.com>
2023-01-20 19:18:49 +0800

    Update color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         }}
       >
         <motion.h1
-          className="bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent drop-shadow-sm md:text-7xl md:leading-[5rem]"
+          className="bg-gradient-to-br from-browny-brown to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent drop-shadow-sm md:text-7xl md:leading-[5rem]"
           variants={FADE_DOWN_ANIMATION_VARIANTS}
         >
           <Balancer>Turn Books into Playlists</Balancer>
