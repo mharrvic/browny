76e72048085c9cc8cbb44b942b18400319a3e224
mac <mharrvic@gmail.com>
2023-01-20 19:15:28 +0800

    Update nextauth to get the access token

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 // Prisma adapter for NextAuth, optional and can be removed
@@ -6,18 +7,103 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db";
 
+/**
+ * Takes a token, and returns a new token with updated
+ * `accessToken` and `accessTokenExpires`. If an error occurs,
+ * returns the old token and an error property
+ */
+async function refreshAccessToken(token: string) {
+  try {
+    console.log("refreshing token...");
+    const url = "https://accounts.spotify.com/api/token";
+
+    const response = await fetch(url, {
+      headers: {
+        Authorization:
+          "Basic " +
+          Buffer.from(
+            `${env.SPOTIFY_CLIENT_ID}:${env.SPOTIFY_CLIENT_SECRET}`
+          ).toString("base64"),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        grant_type: "refresh_token",
+        refresh_token: token,
+      }),
+
+      method: "POST",
+    });
+
+    const refreshedTokens = await response.json();
+
+    if (!response.ok) {
+      throw refreshedTokens;
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      accessTokenExpires: Date.now() + refreshedTokens.expires_at * 1000,
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
+
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
+  jwt: {
+    secret: env.NEXTAUTH_SECRET,
+  },
+  debug: true,
+  session: {
+    strategy: "jwt",
+  },
+
   callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
+    async jwt({ token, account, user, profile }) {
+      console.log("jwt callbacks");
+
+      // Initial sign in
+      if (account) {
+        return {
+          accessToken: account.access_token,
+          accessTokenExpires: Date.now() + account.expires_at * 1000,
+          refreshToken: account.refresh_token,
+          user,
+          profile,
+        };
       }
+
+      // Return previous token if the access token has not expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        return token;
+      }
+
+      // Access token has expired, try to update it
+      return refreshAccessToken(token.refreshToken);
+    },
+
+    async session({ session, token }) {
+      session.accessTokenExpires = token.accessTokenExpires;
+      session.user = token.user;
+      session.profile = token.profile;
+      session.refreshToken = token.refreshToken;
+      session.accessToken = token.accessToken;
+      session.error = token.error;
+
       return session;
     },
   },
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
+  secret: env.NEXTAUTH_SECRET,
   providers: [
     SpotifyProvider({
       clientId: env.SPOTIFY_CLIENT_ID,
